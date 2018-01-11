01e1a2b0b41444a29d3611fb6c05d19e85e31556
osamasayegh <OsamaSayegh@users.noreply.github.com>
2018-01-11 08:41:20 +0300

    initial release

---
+++ plugin.rb
@@ -0,0 +1,41 @@
+# name: discourse-unsolved-filter-modified
+# about: A modified verions of discourse-unsolved-filter to support modified verions of the official Solved plugin
+# version: 0.1
+# authors: Osama Sayegh
+# url: https://github.com/OsamaSayegh/discourse-unsolved-filter-modified
+
+Discourse.filters << :unsolved
+Discourse.anonymous_filters << :unsolved
+
+after_initialize do
+  TopicQuery.class_eval do
+    def list_unsolved
+      create_list(:unsolved) do |topics|
+        topics = topics.where("topics.id NOT IN (
+          SELECT tc.topic_id
+          FROM topic_custom_fields tc
+          WHERE (tc.name = 'accepted_answer_post_id' AND tc.value IS NOT NULL) OR
+          (tc.name = 'accepted_answer_post_ids' AND tc.value IS NOT NULL) OR
+          (tc.name = 'solved_state' AND tc.value = 'solved')
+        ) OR topics.id IN (
+          SELECT tc.topic_id
+          FROM topic_custom_fields tc
+          WHERE tc.name = 'solved_state' AND (tc.value = 'unsolved' OR tc.value = '')
+        )").where("topics.id NOT IN (
+          SELECT cats.topic_id
+          FROM categories cats WHERE cats.topic_id IS NOT NULL
+        )")
+
+        if !SiteSetting.allow_solved_on_all_topics
+          topics = topics.where("topics.category_id IN (
+            SELECT ccf.category_id
+            FROM category_custom_fields ccf
+            WHERE ccf.name = 'enable_accepted_answers' AND
+            ccf.value = 'true'
+          )")
+        end
+        topics
+      end
+    end
+  end
+end
